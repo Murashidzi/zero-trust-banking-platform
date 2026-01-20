4a12af4b29b477fb496ed4f4d7216ea4c604ca22
Murashidzi <murashidzithomas@gmail.com>
2026-01-20 16:39:48 +0200

    Fix: wrapped backend block in terraform block

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-backend "s3" {
-	bucket	= "terraform-state-devsecops-murashidzi"
-	key	= "global/s3/terraform.tfstate"
-	region	= "af-south-1"
-	dynamodb_table = "terraform-locks"
-	encrypt	= true
-	}
+terraform {
 
+	backend "s3" {
+		bucket	= "terraform-state-devsecops-murashidzi"
+		key	= "global/s3/terraform.tfstate"
+		region	= "af-south-1"
+		dynamodb_table = "terraform-locks"
+		encrypt	= true
+	}
 }
